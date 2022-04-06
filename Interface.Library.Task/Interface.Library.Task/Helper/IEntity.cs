fed6a0043602aa91bbc0a09d2e82155c330f58bc
muradav <muradav@code.edu.az>
2022-04-06 17:40:34 +0400

    feat:class parts done. method parts will be written. task not completed

---
+++ Interface.Library.Task/Interface.Library.Task/Helper/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface.Library.Task.Helper
+{
+    internal interface IEntity
+    {
+        int ID { get; }
+    }
+}
---
+++ Interface.Library.Task/Interface.Library.Task/Models/Library.cs
@@ -0,0 +1,28 @@
+﻿using Interface.Library.Task.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface.Library.Task.Models
+{
+    class Library:IEntity
+    {
+        private static int _id;
+        public int ID { get; }
+
+        public int BookLimit { get; set; }
+
+       
+
+        private int Count = 0;
+
+        private List<Books> books;
+
+        public Library()
+        {
+            books = new List<Books>();
+            books.Capacity = BookLimit;
+
+        }
+    }
+}
